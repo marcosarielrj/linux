2705f7afd61eb9643eeba92808805ea976ba8f90
Marcos Ariel Rodrigues de Jesus <marcosariel.rj@hotmail.com>
2022-08-26 14:56:51 -0300

    Update criar-usuarios-lote.sh

--- criar-usuarios-lote.sh
+++ criar-usuarios-lote.sh
@@ -2,16 +2,16 @@
 
 echo "Criando usuários no sistema"
 
-useradd guest5 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt sennha123) - senha atual capital00
+useradd guest5 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt senha123) - senha atual capital00
 passwd guest5 -e
 
-useradd guest6 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt sennha123)
+useradd guest6 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt senha123)
 passwd guest6 -e
 
-useradd guest8 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt sennha123)
+useradd guest8 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt senha123)
 passwd guest8 -e
 
-useradd guest7 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt sennha123)
+useradd guest7 -c "Usuário Convidado" -m -s /bin/bash -p $(openssl passwd -crypt senha123)
 passwd guest7 -e
 
 echo "Finalizado"
